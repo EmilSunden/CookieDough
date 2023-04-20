9cf927abe9e41871c2f4642de5e3ba9dfb8db29e
EmilSunden <emilsunjobb@gmail.com>
2023-04-20 22:21:14 +0200

    Imported authRouter

--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+const { authRouter } = require('./routes/authRoutes/authRoutes');
+app.use('/', authRouter);
+
 const { PORT } = process.env;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost/${PORT}`)
